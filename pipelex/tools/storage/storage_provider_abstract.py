f94feff19032f562916f0a76a431cbc564bdd57d
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@gmail.com>
2025-06-26 13:46:44 +0500

    Changed storage abstract class methods.

--- pipelex/tools/storage/storage_provider_abstract.py
+++ pipelex/tools/storage/storage_provider_abstract.py
@@ -1,13 +1,11 @@
 from abc import ABC, abstractmethod
 
-from botocore.client import BaseClient
-
 
 class StorageProviderAbstract(ABC):
     @abstractmethod
-    def get_object(self, bucket_name: str, object_key: str) -> bytes:
+    def load(self, uri: str) -> bytes:
         pass
 
     @abstractmethod
-    def get_client(self) -> BaseClient:
+    def store(self, data: bytes) -> str:
         pass
